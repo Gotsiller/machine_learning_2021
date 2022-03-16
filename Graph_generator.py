dd9a46944e033415843aaa011eaf2d26a405be4b
Christoph Siller <e1618614@student.tuwien.ac.at>
2022-03-16 16:46:40 +0100

    removes old stuff, improves wine stuff, adds internet usage stuff

--- Graph_generator.py
+++ Graph_generator.py
@@ -1,70 +1,49 @@
+
 import matplotlib.pyplot as plt
 import pandas as pd
 
 ####################### DATASETS ###############################
-# Load csv files
-wine = pd.read_csv('Datasets/white-wine-quality.csv')
-fps = pd.read_csv('Datasets/fps-in-video-games.csv')
-# fps_small = pd.read_csv('Datasets/fps-in-video-games_small.csv')
-
-labels = {'V1': 'fixed acidity',
-          'V2': 'volatile acidity',
-          'V3': 'citric acid',
-          'V4': 'residual sugar',
-          'V5': 'chlorides',
-          'V6': 'free sulfur dioxide',
-          'V7': 'total sulfur dioxide',
-          'V8': 'density',
-          'V9': 'pH',
-          'V10': 'sulphates',
-          'V11': 'alcohol',
-          'Class': 'quality'}
-
-# for white wine:
-acidity = wine[['V1', 'V2', 'V3']]
-sulfur = wine[['V7', 'V6']]
-sugar = wine['V4']
-result = wine['Class']
-split1 = wine[['V11', 'V1', 'V9']]
-split2 = wine[['V2', 'V3', 'V5', 'V8', 'V10']]
-total_split1 = wine[['V7', 'V6', 'V4']]
-total_split2 = wine[['V11', 'V1', 'V9']]
-total_split3 = wine[['V2', 'V3', 'V5', 'V8', 'V10']]
-
-# for FPS
-fps_values = fps['FPS']
-cpu_frequency = fps['CpuFrequency']
-game_name = fps["GameName"].tolist()
-
-# ------------------------------------------------
-gpu_memory_size = fps['GpuMemorySize'].tolist()
-
-# set missing values to 0
-for i in range(0, len(gpu_memory_size)):
-    if gpu_memory_size[i] == "?":
-        gpu_memory_size[i] = 0
-
-gpu_memory_size = list(map(int, gpu_memory_size))
 
 
-# -------------------------------------------------
+def load_white_wine() -> tuple[pd.DataFrame, dict]:
+    wine = pd.read_csv('data/white_wine.csv')
+    labels = {'V1': 'fixed acidity',
+              'V2': 'volatile acidity',
+              'V3': 'citric acid',
+              'V4': 'residual sugar',
+              'V5': 'chlorides',
+              'V6': 'free sulfur dioxide',
+              'V7': 'total sulfur dioxide',
+              'V8': 'density',
+              'V9': 'pH',
+              'V10': 'sulphates',
+              'V11': 'alcohol',
+              'Class': 'quality'}
+    return wine, labels
+
+def load_internet_usage() -> pd.DataFrame:
+    netuse = pd.read_csv('data/internet_usage.csv')
+
+    # exchange columns of "Actual Time" and "who"
+    netuse = netuse.rename(columns = {'Actual Time': 'who', 'who': 'Actual Time'})
+    return netuse
 
 
-# #################### METHODS ########################################
+def remove_missing_values(column: pd.Series, symbol: str) -> pd.Series:
+    return column.drop(column.index[column == symbol])
 
 
-def plot_table_wine(table, plot_name, file_name):
+def plot_table_wine(table, labels, plot_name, file_name):
     for n in table:
         plt.hist(table[n], 100, histtype='step', label=labels[n])
 
     plt.title(plot_name)
     plt.legend()
     plt.savefig(file_name)
-    # plt.show()
     plt.close()
 
 
-def plot_column_wine(column, plot_name, file_name):
+def plot_column_wine(column, labels, plot_name, file_name):
     plt.hist(column, 100, label=labels[column.name])
 
     plt.title(plot_name)
@@ -74,7 +53,7 @@ def plot_column_wine(column, plot_name, file_name):
     plt.close()
 
 
-def plot_column_fps(column, bins, plot_name, file_name):
+def plot_column_netuse(column, bins, plot_name, file_name):
     plt.hist(column, bins)
     plt.title(plot_name)
     plt.xticks(rotation=30)
@@ -84,32 +63,29 @@ def plot_column_fps(column, bins, plot_name, file_name):
     plt.close()
 
 
-def plot_column_fps_nominal(column, plot_name, file_name):
-    plt.figure(figsize=(20, 5), constrained_layout=True)
-    plt.hist(column, 30)
-    plt.title(plot_name)
-    plt.xticks(rotation=60)
-    plt.savefig(file_name)
-    # plt.show()
-    plt.close()
+#
+# def plot_column_fps_nominal(column, plot_name, file_name):
+#     plt.figure(figsize=(20, 5), constrained_layout=True)
+#     plt.hist(column, 30)
+#     plt.title(plot_name)
+#     plt.xticks(rotation=60)
+#     plt.savefig(file_name)
+#     # plt.show()
+#     plt.close()
 
 
-############## HISTOGRAMS ############################################
+if __name__ == '__main__':
+    # -------------- WHITE WINE QUALITY ----------------------------------
+    # wine, labels = load_white_wine()
+    # plot_table_wine(wine, labels, 'White Wine', 'Graphs/WineTotal.png')
+    # plot_column_wine(wine['Class'], labels, 'Quality', 'Graphs/Quality.png')
+    # plot_table_wine(wine[['V7', 'V6', 'V4']], labels, '', 'Graphs/total_split1.png')
+    # plot_table_wine(wine[['V11', 'V1', 'V9']], labels, '', 'Graphs/total_split2.png')
+    # plot_table_wine(wine[['V2', 'V3', 'V5', 'V8', 'V10']], labels, '', 'Graphs/total_split3.png')
 
-# -------------- WHITE WINE QUALITY ----------------------------------
-# plot_table_wine(wine, 'White Wine', 'Graphs/WineTotal.png')
-# plot_table_wine(sulfur, 'White Wine Sulfur', 'Graphs/Sulfur.png')
-# plot_column_wine(result, 'Quality', 'Graphs/Quality.png')
-# plot_column_wine(sugar, 'Sugar', 'Graphs/sugar.png')
-# plot_table_wine(split1, 'Split1', 'Graphs/split1.png')
-# plot_table_wine(split2, 'Split2', 'Graphs/split2.png')
-# plot_table_wine(total_split1, '', 'Graphs/total_split1.png')
-# plot_table_wine(total_split2, '', 'Graphs/total_split2.png')
-# plot_table_wine(total_split3, '', 'Graphs/total_split3.png')
+    # ---------------- INTERNET USAGE -----------------------------------
+    netuse = load_internet_usage()
+    plot_column_netuse(netuse['Actual Time'], 100, 'Internet Usage Time', 'Graphs/ActualTime.png')
 
-
-# ------------- PC GAME FPS --------------------------------------
-# plot_column_fps(fps_values, 50, "FPS", "Graphs/fps.png")
-# plot_column_fps(cpu_frequency, 30, "CPU frequency", "Graphs/cpu")
-# plot_column_fps(gpu_memory_size, 20, "GPU memory size", "Graphs/gpu.png")
-# plot_column_fps_nominal(game_name, "Game Name", "Graphs/games.png")
+    age_column = remove_missing_values(netuse['Age'], 'Not_Say').astype(int)
+    plot_column_netuse(age_column, 50, 'Age of Users', 'Graphs/Age.png')
